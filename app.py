beafd5ee5a9d4e3895804f919583fbf0635debb0
Diksha Tiwari <53514258+dikshatiwari226@users.noreply.github.com>
2026-03-14 16:24:12 +0530

    fixed app.py file lint issue

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(name)
 
 
 @app.route('/')
@@ -11,4 +11,6 @@ def hello_world():
 def health():
     return 'Server is up and running'
 
-app.run(debug=True, host='0.0.0.0', port=80)
+
+if __name__ == '__main__':
+    app.run(port=80)
